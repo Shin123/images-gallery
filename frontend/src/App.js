1a68a0c400820d2ed4886394354da431c92c17d7
shin <vanvietkhtn@gmail.com>
2022-11-02 17:40:03 +0700

    introduce axios and rewrite new-image fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 import Header from './components/Header';
 import Search from './components/Search';
@@ -13,16 +14,16 @@ const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
-  const handleSearchSubmit = (e) => {
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    fetch(`${API_URL}/new-image?query=${word}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setImages([{ ...data, title: word }, ...images]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+    try{
+      const res = await axios.get(`${API_URL}/new-image?query=${word}`)
+      setImages([{ ...res.data, title: word }, ...images]);
+    } catch (error) {
+      console.log(error)
+    }
+
     setWord('');
   };
 
