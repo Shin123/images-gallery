ba2982650777c722dc9705a26102d86af770dd23
shin <vanvietkhtn@gmail.com>
2022-11-03 00:21:58 +0700

    add nav link to header logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-    backgroundColor: '#596886'
-}
+  backgroundColor: '#596886',
+};
+
+const SITE_URL = process.env.REACT_APP_SITE_URL || 'http://127.0.0.1:3000';
 
 const Header = ({ title }) => {
-    return (
-        <Navbar style={navbarStyle} expand="lg" variant="light">
-            <Container>
-                <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }}/>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar style={navbarStyle} expand='lg' variant='light'>
+      <Container>
+        <Nav.Link href={SITE_URL}>
+          <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
+        </Nav.Link>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -7,22 +7,24 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
 
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image.urls.small} />
+      <Card.Img variant='top' src={image.urls.small} />
       <Card.Body>
         <Card.Title>{image.title?.toUpperCase()}</Card.Title>
-        <Card.Text>
-          {image.description || image.alt_description}
-        </Card.Text>
-        <Button variant="primary" onClick={() => deleteImage(image.id)}>Delete</Button>{' '}
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant='primary' onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>{' '}
         {!image.saved && (
-          <Button variant="secondary" onClick={() => saveImage(image.id)}>
+          <Button variant='secondary' onClick={() => saveImage(image.id)}>
             Save
           </Button>
         )}
       </Card.Body>
-      <Card.Footer className="text-center text-muted">
+      <Card.Footer className='text-center text-muted'>
         {authorPortfolioURL && (
-          <Nav.Link href={authorPortfolioURL} target="_blank">{authorName}</Nav.Link>
+          <Nav.Link href={authorPortfolioURL} target='_blank'>
+            {authorName}
+          </Nav.Link>
         )}
         {!authorPortfolioURL && authorName}
       </Card.Footer>
